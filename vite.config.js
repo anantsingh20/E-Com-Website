7f7dd2fd2bb523625a56c82087ea9b832d6f17f6
Anant Singh <anantprataps857@gmail.com>
2025-12-29 16:44:39 +0530

    fix vite base + assets

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,18 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/", // 🔥 THIS IS MANDATORY FOR NETLIFY
+
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"), // Your main entry point (e.g., home page)
-        about: resolve(__dirname, "about.html"), // Additional HTML pages
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
         contact: resolve(__dirname, "contact.html"),
         products: resolve(__dirname, "products.html"),
         addToCart: resolve(__dirname, "addToCart.html"),
-        // Add more entry points for other HTML files as needed
       },
     },
   },
 });
+
