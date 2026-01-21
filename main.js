a56317c9cf975259a2d81412a4c67790f2878cdf
Anant Singh <anantprataps857@gmail.com>
2026-01-21 17:10:10 +0530

    Updated changes

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import "./style.css";
 import products from "./api/products.json";
 import { showProductContainer } from "./homeProductCards";
 
-// Define a function named `showProductContainer` that takes an array of products as input.
 showProductContainer(products);
 
-//todo Don't Forget To LIKE SHARE & SUBSCRIBE TO THAPA TECHNCIAL YOUTUBE CHANNEL 👉 https://www.youtube.com/thapatechnical
+
